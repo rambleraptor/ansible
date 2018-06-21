2114b34c02c21a341e36124ed0aed88c7bc9614c
Alex Stephen <alexstephen@google.com>
2018-06-21 19:40:12 +0000

    Default scopes

--- lib/ansible/modules/cloud/google/gcp_compute_https_health_check.py
+++ lib/ansible/modules/cloud/google/gcp_compute_https_health_check.py
@@ -225,6 +225,9 @@ def main():
         )
     )
 
+    if not module.params['scopes']:
+        module.params['scopes'] = ['https://www.googleapis.com/auth/compute']
+
     state = module.params['state']
     kind = 'compute#httpsHealthCheck'
 
